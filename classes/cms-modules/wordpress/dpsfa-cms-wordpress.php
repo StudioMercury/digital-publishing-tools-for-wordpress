8361cc3cd51f59bcb4d7e27152b47e7d10319bbb
Remo Blaser <remo.blaser@gmail.com>
2015-09-01 15:16:20 +0200

    Check if any terms are returned before iterating through them

--- classes/cms-modules/wordpress/dpsfa-cms-wordpress.php
+++ classes/cms-modules/wordpress/dpsfa-cms-wordpress.php
@@ -474,9 +474,11 @@ if(!class_exists('DPSFolioAuthor\CMS')) {
 			$taxonomy_names = get_object_taxonomies( $post->post_type );
 			foreach($taxonomy_names as $taxonomy){
 				$terms = get_the_terms( $id, $taxonomy );
-				foreach($terms as $term){
-					array_push($internalKeywords, $term->name);
-				}
+                if($terms) {
+    				foreach($terms as $term){
+    					array_push($internalKeywords, $term->name);
+    				}
+                }
 			}
 			
 			$entity->title = $post->post_title;
